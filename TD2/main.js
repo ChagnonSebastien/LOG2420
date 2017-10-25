c77eafdff9dcdd85a84595b7befd7380c9a8403a
Sebastien Chagnon <chagnon.s21@gmail.com>
2017-10-25 09:32:49 -0400

    added the map

--- TD2/main.js
+++ TD2/main.js
@@ -1,3 +1,12 @@
-$(document).ready( function () {
-    $('#table_id').DataTable();
-} );
+function initialize() {
+    var latlng = new google.maps.LatLng(45.5187, -73.5776);
+    var myOptions = {
+        zoom: 10,
+        center: latlng,
+        mapTypeId: google.maps.MapTypeId.SATELLITE
+    };
+    var map = new google.maps.Map(document.getElementById("map"),
+            myOptions);
+}
+
+google.maps.event.addDomListener(window, "load", initialize);
